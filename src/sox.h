60874eda08683764fb3a8a844652bc5d8ccd6a93
zzy <iowrite@icloud.com>
2025-09-16 10:55:20 +0800

    v2.2.1

--- src/sox.h
+++ src/sox.h
@@ -9,7 +9,7 @@
  ******************************************************************************/    
 #define EKF_SOX_VERSION_MAJOR         2
 #define EKF_SOX_VERSION_MINOR         2
-#define EKF_SOX_VERSION_DEBUG         0
+#define EKF_SOX_VERSION_DEBUG         1
  
 
 
