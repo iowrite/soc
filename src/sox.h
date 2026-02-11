9e81e2239c4793612eaaa4a379b2b192d6d79ac4
zzy <iowrite@icloud.com>
2026-02-11 09:46:18 +0800

    修复soh大于100次进行校准

--- src/sox.h
+++ src/sox.h
@@ -9,7 +9,7 @@
  ******************************************************************************/    
 #define EKF_SOX_VERSION_MAJOR         2
 #define EKF_SOX_VERSION_MINOR         4
-#define EKF_SOX_VERSION_PATCH         11
+#define EKF_SOX_VERSION_PATCH         12
  
 
 
